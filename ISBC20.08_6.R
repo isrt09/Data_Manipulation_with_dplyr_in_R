f4d9492a5423f4e9451e6f80e75f9fc3bf90cd3b
mazed <mazedur@gmail.com>
2021-08-04 16:06:43 +0600

    dplyr group by manipulation

---
+++ ISBC20.08_6.R
@@ -0,0 +1,22 @@
+require(dplyr)
+
+# Load Dataset
+data<- read.csv("C:/Users/user/Documents/dplyr/demographics.csv")
+
+glimpse(data)
+
+d1 <- group_by(data, educ)
+
+View(d1)
+
+summarise(d1, avg = mean(income))
+ 
+summarise(d1, std = sd(carpr))
+
+d2 <- filter(d1, age >40)
+
+summarise(d2, avg = mean(income))
+
+summarise(d2, across(c(1,4),mean))
+
+summarise(filter(d2, carcat=="Luxury"), across(where(is.numeric), mean))
